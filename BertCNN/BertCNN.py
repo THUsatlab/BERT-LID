ca3435979366bb7895a835d9082cdc197f89b879
NieYuting <nyt970129@163.com>
2022-11-22 20:40:03 +0800

    Bert model

--- BertCNN/BertCNN.py
+++ BertCNN/BertCNN.py
@@ -19,7 +19,7 @@ class BertCNN(BertPreTrainedModel):
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-        self.convs = Conv1d(80, n_filters, filter_sizes)
+        self.convs = Conv1d(768, n_filters, filter_sizes)
 
         self.classifier = nn.Linear(len(filter_sizes) * n_filters, num_labels)
         self.apply(self.init_bert_weights)
@@ -33,10 +33,13 @@ class BertCNN(BertPreTrainedModel):
             token_type_ids: 区分句子，0 为第一句，1表示第二句
             attention_mask: 区分 padding 与 token， 1表示是token，0 为padding
         """
-        #encoded_layers, _ = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
-        # encoded_layers: [batch_size, seq_len, bert_dim=768]
+        input_ids = input_ids.long()
+        #print('y.dtype: ',input_ids.dtype)
+        encoded_layers, _ = self.bert(
+            input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
+        # encoded_layers: [batch_size, seq_len, bert_dim] #=768]
 
-        encoded_layers = self.dropout(input_ids)
+        #encoded_layers = self.dropout(input_ids)
 
         encoded_layers = encoded_layers.permute(0, 2, 1)
         # encoded_layers: [batch_size, bert_dim=768, seq_len]
@@ -54,11 +57,9 @@ class BertCNN(BertPreTrainedModel):
         logits = self.classifier(cat)
         # logits: [batch_size, output_dim]
 
-        print('pre, true:', logits.view, labels.view(-1), cat)
         if labels is not None:
             loss_fct = CrossEntropyLoss()
             loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
-            print('pre, true:', logits.view(-1, self.num_labels), labels.view(-1))
             return loss
         else:
             return logits
